e3bd20c87774d994d23020fc0979d32a64bea13b
r.zwiers <r.zwiers@iwink.nl>
2016-08-19 11:29:03 +0200

    Innit commit

--- functions.php
+++ functions.php
@@ -5,4 +5,11 @@ if ( !defined( 'ABSPATH' ) ) exit;
 // BEGIN ENQUEUE PARENT ACTION
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
 
+add_action( 'wp_enqueue_scripts', 'add_fonts' );
+function add_fonts() {
+
+	wp_enqueue_style( 'GeosansLight-NM', '//fonts.googleapis.com/css?family=EB+Garamond|Spinnaker', array(), emmet-lite-child );
+	
+}
+
 // END ENQUEUE PARENT ACTION
