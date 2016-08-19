f321a3fdb4e044cd12bc7f28bce44348c0f893ee
r.zwiers <r.zwiers@iwink.nl>
2016-08-19 13:58:43 +0200

    Innit commit

--- functions.php
+++ functions.php
@@ -5,11 +5,15 @@ if ( !defined( 'ABSPATH' ) ) exit;
 // BEGIN ENQUEUE PARENT ACTION
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
 
-add_action( 'wp_enqueue_scripts', 'add_fonts' );
-function add_fonts() {
 
-	wp_enqueue_style( 'GeosansLight-NM', '//fonts.googleapis.com/css?family=EB+Garamond|Spinnaker', array(), emmet-lite-child );
-	
+function login_error_override()
+{
+  return 'Incorrect login details.';
 }
+add_filter('login_errors', 'login_error_override');
 
+function my_custom_login() {
+  echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('stylesheet_directory') . '/style.css" />';
+}
+add_action('login_head', 'my_custom_login');
 // END ENQUEUE PARENT ACTION
